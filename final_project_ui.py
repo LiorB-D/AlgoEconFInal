954655ae74ce7504d28066c3ffeea0160fbb69c7
DeckerBristow <jdb3qn@virginia.edu>
2023-05-03 21:57:32 -0400

    switched to SSG functions

--- final_project_ui.py
+++ final_project_ui.py
@@ -1,6 +1,10 @@
 import pygame
 import numpy as np
 import random
+from shannon_switching_game import ShannonSwitchingGame, Player
+
+
+
 
 # Define adjacency matrix
 adj_matrix = np.array([[0, 1, 1, 0],
@@ -9,30 +13,15 @@ adj_matrix = np.array([[0, 1, 1, 0],
                        [0, 1, 1, 0]
                        ])
 
+#I also tested this matrix to make sure S and T are correctly labeled
+# adj_matrix = np.array([[0,1,1],
+#                       [1,0,0],
+#                       [1,0,0]])
 
 
+#Instantiate SSG class
+SSG = ShannonSwitchingGame(adj_matrix, 0, 1)
 
-
-#function that takes in adjacency matrix indicies and removes the edge
-def remove_edge(node1, node2):
-    global edges
-    global adj_matrix
-    if adj_matrix[node1, node2] != -1:
-        edges.remove((node1, node2))
-        adj_matrix[node1, node2] = 0
-        adj_matrix[node1, node2] = 0 
-        return True
-    return False
-    
-    
-
-#function that takes in adjacency matrix indicies and reinforces the edge
-def reinforce_edge(node1, node2):
-    global adj_matrix
-    if adj_matrix[node1, node2] != -1:
-        adj_matrix[node1, node2] = -1
-        return True
-    return False
 
 
 #function that draws all the nodes
@@ -51,7 +40,7 @@ def draw_nodes():
 def draw_edges():
     global edges
     for edge in edges:
-        if adj_matrix[edge[0], edge[1]] == -1:
+        if SSG.adj_matrix[edge[0], edge[1]] == -1:
             pygame.draw.line(screen, reinforce_edge_color, node_positions[edge[0]], node_positions[edge[1]], 2)
         
         else:
@@ -60,7 +49,7 @@ def draw_edges():
        
        
        
-       
+# Define screen width and height
 screen_width, screen_height = 1000, 800
   
 # Define your node positions
@@ -72,7 +61,15 @@ tpos = (screen_width-50, screen_height/2)
 node_positions.append(spos)
 node_positions.append(tpos)
 
-while len(node_positions) < len(adj_matrix):
+num_nodes = 0
+
+for i in range(len(SSG.adj_matrix[0])):
+    for j in range(len(SSG.adj_matrix[0])):
+        if np.sum(SSG.adj_matrix[i]) > 0 or (SSG.adj_matrix[j][i]) > 0:
+            num_nodes +=1
+            break
+            
+while len(node_positions) < (num_nodes):
         # Generate a random position for the new node
         x = random.randint(50, screen_width-50)
         y = random.randint(50, screen_height-50)
@@ -98,7 +95,7 @@ while len(node_positions) < len(adj_matrix):
 
     
 
-# Define your node and edge colors
+# Define node and edge colors
 node_color = (255, 255, 255)
 s_color = (0, 255, 0)
 edge_color = (255, 255, 255)
@@ -115,9 +112,11 @@ pygame.display.set_caption("Graph Visualization")
 
 # Initialize and draw edges
 edges = []
-for i in range(adj_matrix.shape[0]):
-    for j in range(i, adj_matrix.shape[1]):
-        if adj_matrix[i, j] != 0:
+for i in range(SSG.adj_matrix.shape[0]):
+    for j in range(i, SSG.adj_matrix.shape[1]):
+        if SSG.adj_matrix[i, j] != 0:
+            
+            
             edge = (i, j)
             edges.append(edge)
             pygame.draw.line(screen, edge_color, node_positions[i], node_positions[j], 2)
@@ -134,6 +133,7 @@ pygame.display.flip()
 
 # Wait for the user to close the window
 player1_turn = True
+player2_turn = False
 running = True
 while running:
     for event in pygame.event.get():
@@ -150,17 +150,33 @@ while running:
                                node2_pos[0] * node1_pos[1] - node2_pos[1] * node1_pos[0]) / \
                            np.sqrt((node2_pos[1] - node1_pos[1]) ** 2 +
                                    (node2_pos[0] - node1_pos[0]) ** 2)
-                # If the distance is less than a certain threshold, remove the edge
+                           
+                           
+                # If the distance is less than a certain threshold, accept the click
                 if distance < 3:
-                    
-                    if player1_turn:                                        
-                        removed = remove_edge(edge[0], edge[1])
-                        if removed:
+                    #if it's player 1's turn make a cut                    
+                    if player1_turn==True and (player2_turn==False):                                        
+                        reward = SSG._ShannonSwitchingGame__cut(edge[0], edge[1])
+                        print(reward)
+                        edges.remove((edge[0], edge[1]))
+                        if reward == (-1, 1):
+                            print("Cutter Wins")
+                            player1_turn, player2_turn = False, False
+                        elif reward != (0,-1):
                             player1_turn = False
-                    else:
-                        reinforced = reinforce_edge(edge[0], edge[1])
-                        if reinforced:
+                            player2_turn = True
+                            
+                    #if it's player 2's turn reinforce 
+                    elif player2_turn==True and (player1_turn==False):
+                        reward = SSG._ShannonSwitchingGame__fix(edge[0], edge[1])
+                        print((reward))
+                        if reward == (1, -1):
+                            print("Fixer Wins")
+                            player1_turn, player2_turn = False, False
+                        elif reward != (-1, 0):
                             player1_turn = True
+                            player2_turn = False
+                            
                     
                     # Clear the screen and redraw everything except the removed edge
                     screen.fill((0, 0, 0))
@@ -178,4 +194,7 @@ while running:
                             
                     
                     pygame.display.flip()
+                    
+                    
                     break 
+                
